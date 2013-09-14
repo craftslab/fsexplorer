5df744a9fec7bb8f9693cd6afd5fbc098b71a388
lemonjia <angersax@gmail.com>
2013-09-14 12:46:49 +0800

    [yafuse-fork]: refactor header of fs based on fs in linux kernel.

--- yafuse-fork/include/fs.h
+++ yafuse-fork/include/fs.h
@@ -35,49 +35,142 @@
 /*
  * Macro Definition
  */
-#define FS_OPT_CMD_MOUNT   "mount"
-#define FS_OPT_CMD_UMOUNT  "umount"
-#define FS_OPT_CMD_STATS   "stats"
-#define FS_OPT_CMD_STAT    "stat"
-#define FS_OPT_CMD_PWD     "pwd"
-#define FS_OPT_CMD_CD      "cd"
-#define FS_OPT_CMD_LS      "ls"
-#define FS_OPT_CMD_MKDIR   "mkdir"
-#define FS_OPT_CMD_RM      "rm"
-#define FS_OPT_CMD_CAT     "cat"
-#define FS_OPT_CMD_ECHO    "echo"
-
-#define FS_CURRENT_PATH  "."
-#define FS_UPPER_PATH    ".."
-#define FS_ROOT_PATH  "/"
-#define FS_PATH_DELIM  "/"
 
 /*
  * Type Definition
  */
-typedef int32_t (*fs_opt_handle_t)(int32_t argc, const char **argv);
+struct timespec;
+struct vfsmount;
+struct mount;
+struct file_system_type;
+struct super_block;
+struct dentry;
+struct inode;
+struct file_operations;
+struct super_operations;
+struct dentry_operations;
+struct inode_operations;
 
-typedef struct {
-  const char *opt_cmd;
-  fs_opt_handle_t opt_hdl;
-} fs_opt_t;
+struct timespec {
+  uint32_t tv_sec;
+  uint32_t tv_nsec;
+};
 
-typedef struct {
-  fs_opt_t opt_mount;
-  fs_opt_t opt_umount;
-  fs_opt_t opt_stats;
-  fs_opt_t opt_stat;
-  fs_opt_t opt_pwd;
-  fs_opt_t opt_cd;
-  fs_opt_t opt_ls;
-  fs_opt_t opt_mkdir;
-  fs_opt_t opt_rm;
-  fs_opt_t opt_cat;
-  fs_opt_t opt_echo;
-} fs_opt_tbl_t;
+struct vfsmount {
+  struct dentry *mnt_root;
+  struct super_block *mnt_sb;
+  int32_t mnt_flags;
+};
 
-#define FS_OPT_TBL_LEN  (sizeof(fs_opt_tbl_t) / sizeof(fs_opt_t))
-#define FS_OPT_TBL_OFFSET  (sizeof(fs_opt_t))
+struct mount {
+  struct dentry *mnt_mountpoint;
+  struct vfsmount mnt;
+  int32_t mnt_count;
+  const char *mnt_devname;
+};
+
+struct file_system_type {
+  const char *name;
+  int32_t fs_flags;
+  struct dentry *(*mount)(struct file_system_type *type, int32_t flags,
+                           const char *name, void *data);
+  int32_t do_umount(struct mount *mnt, int32_t flags);
+};
+
+struct super_block {
+  uint8_t                        s_blocksize_bits;
+  uint32_t                       s_blocksize;
+  int32_t                       s_maxbytes;
+  struct file_system_type        *s_type;
+  const struct super_operations  *s_op;
+  uint32_t                       s_flags;
+  uint32_t                       s_magic;
+  struct dentry                  *s_root;
+  int32_t                        s_count;
+  char                           s_id[32];
+  uint8_t                        s_uuid[16];
+  const struct dentry_operations *s_d_op;
+};
+
+struct dentry {
+  struct dentry       *d_parent;
+  const unsigned char *d_name;
+  struct inode        *d_inode;
+  unsigned char       d_iname[DNAME_INLINE_LEN];
+  struct super_block  *d_sb;
+};
+
+struct inode {
+  uint8_t                       i_mode;
+  uint16_t                      i_opflags;
+  uint32_t                      i_uid;
+  uint32_t                      i_gid;
+  uint32_t                      i_flags;
+  const struct inode_operations *i_op;
+  struct super_block            *i_sb;
+  uint32_t                      i_ino;
+  int32_t                      i_size;
+  struct timespec               i_atime;
+  struct timespec               i_mtime;
+  struct timespec               i_ctime;
+  uint16_t                      i_bytes;
+  uint32_t                      i_blkbits;
+  uint32_t                      i_blocks;
+  uint32_t                      i_version;
+  uint32_t                      i_count;
+  const struct file_operations  *i_fop;
+};
+
+struct file_operations {
+  ssize_t (*read) (struct file *, char *, size_t, int32_t *);
+  ssize_t (*write) (struct file *, const char *, size_t, int32_t *);
+  int32_t (*open) (struct inode *, struct file *);
+  int32_t (*release) (struct inode *, struct file *);
+  int32_t (*show_fdinfo)(struct seq_file *m, struct file *f);
+};
+
+struct super_operations {
+  struct inode *(*alloc_inode)(struct super_block *sb);
+  void (*destroy_inode)(struct inode *);
+  int32_t (*write_inode) (struct inode *, struct writeback_control *wbc);
+  int32_t (*statfs) (struct dentry *, struct kstatfs *);
+  int32_t (*show_devname)(struct seq_file *, struct dentry *);
+  int32_t (*show_path)(struct seq_file *, struct dentry *);
+  int32_t (*show_stats)(struct seq_file *, struct dentry *);
+};
+
+struct dentry_operations {
+  int32_t (*d_hash)(const struct dentry *, unsigned char *);
+  int32_t (*d_delete)(const struct dentry *);
+  char *(*d_dname)(struct dentry *, char *, int32_t);
+};
+
+struct inode_operations {
+  struct dentry * (*lookup) (struct inode *, struct dentry *, uint32_t);
+  void * (*follow_link) (struct dentry *, struct nameidata *);
+  int32_t (*permission) (struct inode *, int32_t);
+  struct posix_acl * (*get_acl)(struct inode *, int32_t);
+  int32_t (*readlink) (struct dentry *, char *,int32_t);
+  void (*put_link) (struct dentry *, struct nameidata *, void *);
+  int32_t (*create) (struct inode *, struct dentry *, uint8_t, bool);
+  int32_t (*link) (struct dentry *, struct inode *, struct dentry *);
+  int32_t (*unlink) (struct inode *, struct dentry *);
+  int32_t (*symlink) (struct inode *, struct dentry *, const char *);
+  int32_t (*mkdir) (struct inode *, struct dentry *, uint8_t);
+  int32_t (*rmdir) (struct inode *, struct dentry *);
+  int32_t (*mknod) (struct inode *, struct dentry *, uint8_t,dev_t);
+  int32_t (*rename) (struct inode *, struct dentry *, struct inode *, struct dentry *);
+  int32_t (*setattr) (struct dentry *, struct iattr *);
+  int32_t (*getattr) (struct vfsmount *mnt, struct dentry *, struct kstat *);
+  int32_t (*setxattr) (struct dentry *, const char *, const void *, size_t, int32_t);
+  ssize_t (*getxattr) (struct dentry *, const char *, void *, size_t);
+  ssize_t (*listxattr) (struct dentry *, char *, size_t);
+  int32_t (*removexattr) (struct dentry *, const char *);
+  int32_t (*update_time)(struct inode *, struct timespec *, int32_t);
+  int32_t (*atomic_open)(struct inode *, struct dentry *,
+                         struct file *, uint32_t open_flag,
+                         uint8_t create_mode, int32_t *opened);
+};
 
 /*
  * Function Declaration
