089c28bba4698a138735cc113e22bb36f9edd115
lemonjia <angersax@gmail.com>
2014-02-18 06:45:48 +0800

    [fs-explorer]: fix incorrect order of item in tree view according                to order in FS.

--- fs-explorer/src/mainwindow.cpp
+++ fs-explorer/src/mainwindow.cpp
@@ -373,8 +373,8 @@ void MainWindow::createTreeItems(unsigned long long ino)
 
   QModelIndex index = treeView->selectionModel()->currentIndex();
 
-  for (unsigned int i = 0; i < childsNum; ++i) {
-    struct fs_dirent child = fsEngine->getFileChilds(i);
+  for (int i = (int)childsNum - 1; i >= 0; --i) {
+    struct fs_dirent child = fsEngine->getFileChilds((unsigned int)i);
     if (child.d_type != FT_DIR
         || !strcmp((const char *)child.d_name, (const char *)FS_DNAME_DOT)
         || !strcmp((const char *)child.d_name, (const char *)FS_DNAME_DOTDOT)) {
