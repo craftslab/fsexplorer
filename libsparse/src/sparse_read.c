88996e71b4ed45b769dc373bab7eb38c317137c7
angersax <angersax@gmail.com>
2015-02-03 22:36:29 +0800

    [fs-explorer/libsparse]: refactor code only.

--- libsparse/src/sparse_read.c
+++ libsparse/src/sparse_read.c
@@ -520,33 +520,32 @@ struct sparse_file *sparse_file_import_auto(int fd, bool crc)
 
 struct sparse_file *zte_sparse_file_import_auto(int fd, bool crc)
 {
-        struct sparse_file *s;
-        int64_t len;
-        int ret;
+	struct sparse_file *s;
+	int64_t len;
+	int ret;
 
-        s = sparse_file_import(fd, false, crc);
-        if (s) {
-                return s;
-        }
+	s = sparse_file_import(fd, false, crc);
+	if (s) {
+		return s;
+	}
 
-        len = lseek64(fd, 0, SEEK_END);
-        if (len < 0) {
-                return NULL;
-        }
+	len = lseek64(fd, 0, SEEK_END);
+	if (len < 0) {
+		return NULL;
+	}
 
-        lseek64(fd, 0, SEEK_SET);
+	lseek64(fd, 0, SEEK_SET);
 
-        s = sparse_file_new(4096, len);
-        if (!s) {
-                return NULL;
-        }
+	s = sparse_file_new(4096, len);
+	if (!s) {
+		return NULL;
+	}
 
-        ret = sparse_file_read_normal(s, fd);
-        if (ret < 0) {
-                sparse_file_destroy(s);
-                return NULL;
-        }
+	ret = sparse_file_read_normal(s, fd);
+	if (ret < 0) {
+		sparse_file_destroy(s);
+		return NULL;
+	}
 
-        return s;
+	return s;
 }
-
